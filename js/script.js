d7c60af061b38922d17b5580dc4802b7a29452dc
2002lyon <leeonsingh20@gmail.com>
2022-10-25 11:15:12 +0545

    mobile responsive

---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+const hamburger = document.querySelector(".hamburger");
+
+hamburger.onclick = function () {
+    const links = document.querySelector(".links");
+    const overlay = document.querySelector(".overlay");
+
+    links.classList.toggle("sticky");
+    overlay.classList.add("sticky");
+}
+
+window.addEventListener("scroll", function (){ 
+    const navbar = document.querySelector("#navigation");
+    navbar.classList.toggle("sticky", window.scrollY > 150)
+})
+
+
+
+
+const overlay = document.querySelector(".overlay");
+overlay.onclick = function() {
+    const links = document.querySelector(".links");
+    links.classList.remove("sticky");
+    overlay.classList.remove("sticky");
+}
+
